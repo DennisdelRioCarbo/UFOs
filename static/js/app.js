4e3a1be671aaead2d2559e216a33f1cdda490630
DennisdelRioCarbo <104289098+DennisdelRioCarbo@users.noreply.github.com>
2022-07-19 18:14:28 -0700

    updated files

--- static/js/app.js
+++ static/js/app.js
@@ -21,7 +21,7 @@ function buildTable(data) {
       cell.text(val);
     });
   });
-}
+};
 
 // 1. Create a variable to keep track of all the filters as an object.
 var filters={};
@@ -50,32 +50,31 @@ function updateFilters() {
     }
   
     console.log(filters);
+    
     // 6. Call function to apply all filters and rebuild the table
     filterTable();
       
-  }
+  };
   
   // 7. Use this function to filter the table when data is entered.
   function filterTable() {
   
      // 8. Set the filtered data to the tableData.
-     let filteredData=tableData;
+      let filteredData=tableData;
      
      // 9. Loop through all of the filters and keep any data that
      // matches the filter values
-     for (var i=0; i<filters.length; i++)
-      
-      
-      let matchedData=filters.property("value");
-     }
-      if (filters[i]){
+      Object.entries(filters).forEach(([key,val])=>{
+        console.log(key,val);
+        filteredData=filteredData.filter(row=>row[key]===val);
+        
+      });
+    
+      console.log(filteredData);
 
-        filteredData=filteredData.filter(row=>row.input===matchedData);
-      };
-      
      // 10. Finally, rebuild the table using the filtered data
-     buildTable(filteredData);
-   
+      buildTable(filteredData);
+  };
   
   // 2. Attach an event to listen for changes to each filter
   d3.selectAll("input").on("change", updateFilters);
